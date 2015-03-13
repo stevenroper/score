557325e67a3850ac7e0e0f2855ea17cc62cce802
Steven Roper <steven.m.roper@gmail.com>
2015-03-12 19:00:44 -0600

    Simplifying New Game creation (for now).

--- js/newGame/NewGameController.js
+++ js/newGame/NewGameController.js
@@ -1,12 +1,14 @@
 var app = angular.module('scoreApp');
 
-app.controller('NewGameController', function($scope, newGameService) {
+app.controller('NewGameController', function($scope, newGameService, $firebaseArray) {
 	
+	$scope.games = $firebaseArray(new Firebase('https://myscore.firebaseio.com/scoreApp/games'));
 	//create player
 	
 	//create new game
 	$scope.createNewGame = function() {
-		newGameService.createNewGame();
+		newGameService.createNewGame($scope.newGame.pointsGood, $scope.newGame.endingScore);
+		
 	};
 
 });
