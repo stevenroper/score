cea3937e50053eaf60c54725c37eb4bbe395fe7e
Steven Roper <steven.m.roper@gmail.com>
2015-03-14 10:29:41 -0600

    Cleaning up New Game Controller code.

--- js/newGame/NewGameController.js
+++ js/newGame/NewGameController.js
@@ -1,19 +1,10 @@
 var app = angular.module('scoreApp');
 
 app.controller('NewGameController', function($scope, newGameService, $firebaseArray, $firebaseObject, newPlayerService) {
-	
-	$scope.games = $firebaseArray(new Firebase('https://myscore.firebaseio.com/scoreApp/games/'));
 
-	$scope.games.$loaded().then(function(data) {
-		console.log(data.$getRecord("otmwxg"));
-	});
-
-	//create new game
 	$scope.createNewGame = function() {
 		var gameId = newGameService.createNewGame($scope.newGame.pointsGood, $scope.newGame.endingScore);
 		newPlayerService.createNewPlayer($scope.newPlayer.name, gameId);
 	};
-
-	//create player
 
 });
