7afa41313c96b32345d3eddcb53a67c1fc132bc2
Steven Roper <steven.m.roper@gmail.com>
2015-03-14 10:39:12 -0600

    Now will add a new player to the game.

--- js/joinGame/JoinGameController.js
+++ js/joinGame/JoinGameController.js
@@ -1,11 +1,12 @@
 var app = angular.module('scoreApp');
 
-app.controller('JoinGameController', function($scope, newPlayerService, joinGameService) {
+app.controller('JoinGameController', function($scope, joinGameService) {
 	
 	$scope.joinGame = function() {
 		joinGameService.checkAccessCode($scope.joinGameForm.accessCode).then(function(canFindGame) {
 			if(canFindGame) {
 				console.log('Woop! Found your game. Let\'s play!');
+				joinGameService.addPlayer($scope.joinGameForm.accessCode, $scope.joinGameForm.name);
 			} else {
 				console.log('Access code is invald. Try again, or create a new game.');
 			}	
--- js/joinGame/joinGameService.js
+++ js/joinGame/joinGameService.js
@@ -1,6 +1,6 @@
 var app = angular.module('scoreApp');
 
-app.service('joinGameService', function($firebaseArray, $q) {
+app.service('joinGameService', function($firebaseArray, $q, newPlayerService) {
 
 	this.checkAccessCode = function(code) {
 		var gamesRef = $firebaseArray(new Firebase('https://myscore.firebaseio.com/scoreApp/games'));
@@ -17,6 +17,10 @@ app.service('joinGameService', function($firebaseArray, $q) {
 		});
 
 		return deferred.promise;
+	};
+
+	this.addPlayer = function(gameId, playerName) {
+		newPlayerService.createNewPlayer(playerName, gameId);
 	};
 
 });
