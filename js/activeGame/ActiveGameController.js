6131e28af76c30f5b66ca42717173009fcbb292d
Steven Roper <steven.m.roper@gmail.com>
2015-03-14 12:18:07 -0600

    Binding data to view.

--- js/activeGame/ActiveGameController.js
+++ js/activeGame/ActiveGameController.js
@@ -1,5 +1,18 @@
 var app = angular.module('scoreApp');
 
-app.controller('ActiveGameController', function($scope, $routeParams) {
-	console.log($routeParams.gameId);
+app.controller('ActiveGameController', function($scope, $routeParams, gameService, $firebaseObject, $firebaseArray) {
+	$scope.gameId = $routeParams.gameId;
+	var gameRef = $firebaseObject(new Firebase('https://myscore.firebaseio.com/scoreApp/games/' + $scope.gameId));
+
+	gameRef.$loaded().then(function(game) {
+		gameRef.$bindTo($scope, 'game');
+	});
+	
+
+	
+
+	// gameService.getGameData($scope.gameId).then(function(game) {
+	// 	console.log(game);
+	// });
+
 });
