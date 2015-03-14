9fd12a1cc0f138620f01d4aa86e15afc3714b12f
Steven Roper <steven.m.roper@gmail.com>
2015-03-14 10:47:12 -0600

    Adding path rediect to active game view.

--- js/joinGame/JoinGameController.js
+++ js/joinGame/JoinGameController.js
@@ -1,12 +1,13 @@
 var app = angular.module('scoreApp');
 
-app.controller('JoinGameController', function($scope, joinGameService) {
+app.controller('JoinGameController', function($scope, joinGameService, $location) {
 	
 	$scope.joinGame = function() {
 		joinGameService.checkAccessCode($scope.joinGameForm.accessCode).then(function(canFindGame) {
 			if(canFindGame) {
 				console.log('Woop! Found your game. Let\'s play!');
 				joinGameService.addPlayer($scope.joinGameForm.accessCode, $scope.joinGameForm.name);
+				$location.path('/games/' + $scope.joinGameForm.accessCode);
 			} else {
 				console.log('Access code is invald. Try again, or create a new game.');
 			}	
--- js/newGame/NewGameController.js
+++ js/newGame/NewGameController.js
@@ -1,10 +1,11 @@
 var app = angular.module('scoreApp');
 
-app.controller('NewGameController', function($scope, newGameService, $firebaseArray, $firebaseObject, newPlayerService) {
+app.controller('NewGameController', function($scope, $location, newGameService, newPlayerService) {
 
 	$scope.createNewGame = function() {
 		var gameId = newGameService.createNewGame($scope.newGame.pointsGood, $scope.newGame.endingScore);
 		newPlayerService.createNewPlayer($scope.newPlayer.name, gameId);
+		$location.path('/games/' + gameId);
 	};
 
 });
