525ee15a069772b51a771ce8b95d5f7e237ac295
Steven Roper <steven.m.roper@gmail.com>
2015-03-18 18:18:49 -0600

    Adding calculator (add & subtract) to Update Score modal.

--- js/activeGame/ActiveGameController.js
+++ js/activeGame/ActiveGameController.js
@@ -3,6 +3,7 @@ var app = angular.module('scoreApp');
 app.controller('ActiveGameController', function($scope, $routeParams, $firebaseObject, $firebaseArray, activeGameService) {
 	$scope.gameId = $routeParams.gameId;
 	var gameRef = $firebaseArray(new Firebase('https://myscore.firebaseio.com/scoreApp/games/' + $scope.gameId));
+	$scope.calcFunc = 'add';
 
 	gameRef.$loaded().then(function(game) {
 		$scope.game = gameRef.$getRecord('players');
@@ -10,8 +11,9 @@ app.controller('ActiveGameController', function($scope, $routeParams, $firebaseO
 	});
 	
 	$scope.updateScore = function() {
-		activeGameService.updateScore($scope.newScore);
-		$scope.newScore = '';
+		activeGameService.updateScore($scope.newPoints, $scope.calcFunc);
+		$scope.newPoints = '';
+		$scope.calcFunc = 'add';
 	};
 
 });
--- js/activeGame/activeGameService.js
+++ js/activeGame/activeGameService.js
@@ -2,8 +2,18 @@ var app = angular.module('scoreApp');
 
 app.service('activeGameService', function($window) {
 
-	this.updateScore = function(newScore) {
+	this.updateScore = function(newPoints, calcFunc) {
 		var playerRef = new Firebase($window.sessionStorage.getItem('playerId'));
+		var newScore = 0;
+
+		playerRef.once('value', function(dataSnapshot) {
+			if(calcFunc === 'add') {
+				newScore = dataSnapshot.val().score + newPoints;
+			} else if(calcFunc === 'minus') {
+				newScore = dataSnapshot.val().score - newPoints;
+			}
+		});
+
 		playerRef.update({
 			score: newScore
 		});
