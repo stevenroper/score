a7e81ca70c4ca4409e3606b01d7b0ae2ecfba35c
Steven Roper <steven.m.roper@gmail.com>
2015-03-19 19:02:09 -0600

    Setting radio buttons to default to Good on New Game view.

--- js/newGame/NewGameController.js
+++ js/newGame/NewGameController.js
@@ -2,6 +2,9 @@ var app = angular.module('scoreApp');
 
 app.controller('NewGameController', function($scope, $location, newGameService, newPlayerService) {
 
+	$scope.newGame = {};
+	$scope.newGame.pointsGood = 'true';
+
 	$scope.createNewGame = function() {
 		var gameId = newGameService.createNewGame($scope.newGame.pointsGood, $scope.newGame.endingScore);
 		newPlayerService.createNewPlayer($scope.newPlayer.name, gameId);
