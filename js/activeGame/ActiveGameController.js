9d2c8033f72a92d6aa7d4e45604edb69e002de38
Steven Roper <steven.m.roper@gmail.com>
2015-03-20 20:49:04 -0600

    Getting ordering to work on player cards in active game view.

--- js/activeGame/ActiveGameController.js
+++ js/activeGame/ActiveGameController.js
@@ -2,14 +2,24 @@ var app = angular.module('scoreApp');
 
 app.controller('ActiveGameController', function($scope, $routeParams, $firebaseObject, $firebaseArray, activeGameService) {
 	$scope.gameId = $routeParams.gameId;
-	var gameRef = $firebaseArray(new Firebase('https://myscore.firebaseio.com/scoreApp/games/' + $scope.gameId));
+	var pointsGoodRef = new Firebase('https://myscore.firebaseio.com/scoreApp/games/' + $scope.gameId + "/pointsGood");
+	var players = $firebaseArray(new Firebase('https://myscore.firebaseio.com/scoreApp/games/' + $scope.gameId + "/players"));
+	
 	$scope.calcFunc = 'add';
 
-	gameRef.$loaded().then(function(game) {
-		$scope.game = gameRef.$getRecord('players');
-		//console.log(gameRef);
+	pointsGoodRef.on('value', function(snapshot) {
+		if(snapshot.val() === 'true') {
+			$scope.order = true;
+		} else if(snapshot.val() === 'false') {
+			$scope.order = false;
+		}
+		
 	});
-	
+
+	players.$loaded().then(function(game) {
+		$scope.players = players;
+	});
+
 	$scope.updateScore = function() {
 		activeGameService.updateScore($scope.newPoints, $scope.calcFunc);
 		$scope.newPoints = '';
