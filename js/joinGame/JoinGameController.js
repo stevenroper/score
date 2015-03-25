75f2e87ea1be6dfc45911c7ae753409899d773c5
Steven Roper <steven.m.roper@gmail.com>
2015-03-24 19:07:50 -0600

    Adding error message for if game isn't found.

--- js/joinGame/JoinGameController.js
+++ js/joinGame/JoinGameController.js
@@ -5,11 +5,10 @@ app.controller('JoinGameController', function($scope, joinGameService, $location
 	$scope.joinGame = function() {
 		joinGameService.checkAccessCode($scope.joinGameForm.accessCode).then(function(canFindGame) {
 			if(canFindGame) {
-				console.log('Woop! Found your game. Let\'s play!');
 				joinGameService.addPlayer($scope.joinGameForm.accessCode, $scope.joinGameForm.name);
 				$location.path('/games/' + $scope.joinGameForm.accessCode);
 			} else {
-				console.log('Access code is invald. Try again, or create a new game.');
+				$('.error').fadeIn(500).fadeOut(5000);
 			}	
 		});
 	};
