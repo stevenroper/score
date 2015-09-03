94d157260d86a42b4da7d1fcd23cb7c7998d8a4a
Steven Roper <steven.m.roper@gmail.com>
2015-09-02 21:34:27 -0600

    Forcing all access codes in JoinGameController to lower case.

--- js/joinGame/JoinGameController.js
+++ js/joinGame/JoinGameController.js
@@ -3,10 +3,10 @@ var app = angular.module('scoreApp');
 app.controller('JoinGameController', function($scope, joinGameService, $location) {
 	
 	$scope.joinGame = function() {
-		joinGameService.checkAccessCode($scope.joinGameForm.accessCode).then(function(canFindGame) {
+		joinGameService.checkAccessCode($scope.joinGameForm.accessCode.toLowerCase()).then(function(canFindGame) {
 			if(canFindGame) {
-				joinGameService.addPlayer($scope.joinGameForm.accessCode, $scope.joinGameForm.name);
-				$location.path('/games/' + $scope.joinGameForm.accessCode);
+				joinGameService.addPlayer($scope.joinGameForm.accessCode.toLowerCase(), $scope.joinGameForm.name);
+				$location.path('/games/' + $scope.joinGameForm.accessCode.toLowerCase());
 			} else {
 				$('.error').slideDown(1000);
 				setTimeout(function() {
